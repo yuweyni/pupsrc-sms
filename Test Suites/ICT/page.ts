8e3b5e0a111d5b337a38669317f09829729e1f6b
yuweyni <aeyuannebladroma@iskolarngbayan.pup.edu.ph>
2024-06-17 15:49:04 +0800

    commit

---
+++ Test Suites/ICT/page.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>page</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5d172648-a0f3-44ca-aa6a-37737461b08a</testSuiteGuid>
+</TestSuiteEntity>
