568baab8ee4d84be1c87a55033501ea3cb4ef4fb
Vina <hervinafalla@gmail.com>
2024-06-17 12:23:25 +0800

    commit

---
+++ Test Suites/ICT/ict2.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ict2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7bb1c694-954c-4c12-9d6d-849a634b0c78</testSuiteGuid>
+</TestSuiteEntity>
