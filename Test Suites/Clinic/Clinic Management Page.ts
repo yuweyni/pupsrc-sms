eae8e679c95a55e56fa66816f55b5a898bcb616f
yuweyni <aeyuannebladroma@iskolarngbayan.pup.edu.ph>
2024-06-17 12:53:54 +0800

    commit

---
+++ Test Suites/Clinic/Clinic Management Page.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Clinic Management Page</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>997f8d0f-4542-4a54-8343-1b9da8565297</testSuiteGuid>
+</TestSuiteEntity>
