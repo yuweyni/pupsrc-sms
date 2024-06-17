0d0ceb0a9e34c3cef069b299107dfa62b4c64d8d
yuweyni <aeyuannebladroma@iskolarngbayan.pup.edu.ph>
2024-06-17 13:17:19 +0800

    initial commit

---
+++ Test Suites/Room Reservation/Chart of Schedules Page.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Chart of Schedules Page</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>735158a2-d860-43a3-9087-075bf78624c7</testSuiteGuid>
+</TestSuiteEntity>
---
+++ Test Suites/Room Reservation/Room Reservation Form Page.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Room Reservation Form Page</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>52288a65-69bb-4f2e-8795-72d627e4158f</testSuiteGuid>
+</TestSuiteEntity>
