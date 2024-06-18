7dd2711c684c51159676e1232f7ee0bbe0ebcf8a
Jheggy <jhegsardales@iskolarngbayan.pup.edu.ph>
2024-06-18 15:34:37 +0800

    CON- Added Test Cases for Configuration Module

---
+++ Object Repository/Page_PUPSRC-SMS/div_System Settings.rs
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_System Settings</name>
+   <tag></tag>
+   <elementGuidId>adfcbd15-d583-4b5c-b130-b702d397bd74</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='dropdown-card']/div/a[2]/div/div/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;System Settings&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>c9cb0152-b317-4a99-a227-ed545657d59b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>col</value>
+      <webElementGuid>95476735-9274-44e0-a22f-56bd7d13e43c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                                            
+                                                
+                                                    
+                                                
+                                            
+                                            System Settings
+                                        </value>
+      <webElementGuid>2a8daaef-2fdd-4670-9675-d8157ca42b72</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;dropdown-card&quot;)/div[1]/a[@class=&quot;dropdown-item item&quot;]/div[@class=&quot;d-flex flex-column justify-content-center px-4&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col&quot;]</value>
+      <webElementGuid>525e99cb-427c-4f1d-9b60-393f21f4f5e6</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='dropdown-card']/div/a[2]/div/div/div</value>
+      <webElementGuid>343b19a3-0485-499d-8196-7b7528840cc6</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Profile Details'])[1]/following::div[3]</value>
+      <webElementGuid>b6e71c7c-c68d-460a-b2bf-74d69061e28d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Dashboard'])[2]/following::div[11]</value>
+      <webElementGuid>caf023c7-fb01-429b-9bcb-bdba112d1704</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Logout'])[1]/preceding::div[1]</value>
+      <webElementGuid>151496f4-b685-4819-896c-99e301578464</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//a[2]/div/div/div</value>
+      <webElementGuid>4c7a3f63-01b9-408a-a102-99caa36ba3b8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+                                            
+                                                
+                                                    
+                                                
+                                            
+                                            System Settings
+                                        ' or . = '
+                                            
+                                                
+                                                    
+                                                
+                                            
+                                            System Settings
+                                        ')]</value>
+      <webElementGuid>d7db310c-1c39-4e34-8d6b-7bd3e07014a7</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
