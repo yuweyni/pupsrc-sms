fd0445b162d01dcd2a867d9da8afaa4f02c6606d
Vina <hervinafalla@gmail.com>
2024-06-18 14:37:35 +0800

    RDRM - Added Test Suite in the Registrar Folder

---
+++ Test Suites/Registrar/Dashboard.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>This contains all the elements that can be found in the Dashboard of Registrar Module.</description>
+   <name>Dashboard</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>77139ad9-2fd4-4bd8-9116-bc88de13c9d2</testSuiteGuid>
+</TestSuiteEntity>
